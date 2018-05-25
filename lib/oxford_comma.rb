424ff6fd08ed2a272ded7209f3b9911564b11b68
Aziz Kosber <a.kosber@gmail.com>
2018-05-25 21:09:49 +0000

    automatically backed up by learn

---
+++ lib/oxford_comma.rb
@@ -0,0 +1,7 @@
+require 'pry'
+veggie = ["arugula"]
+fruits = ["kiwi", "strawberries"]
+dogs = ["beagle", "rhodesian ridgeback", "german shepherd", "border collie"]
+def oxford_comma(array)
+  
+end
