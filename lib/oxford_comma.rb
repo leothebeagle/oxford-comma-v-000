89652557a8e6af7e4e7b3bfdd269103a6fc40f23
Aziz Kosber <a.kosber@gmail.com>
2018-05-25 21:13:44 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -8,7 +8,7 @@ def oxford_comma(array)
   elsif array.length == 2
     array.join(" and ")
   else
-    array.last = "and #{array.last}"
+    array[-1] = "and #{array[-1]}"
     array.join(", ")
   end
 end
