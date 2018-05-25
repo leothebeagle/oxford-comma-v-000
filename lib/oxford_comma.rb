35dfa9a62a7d0c2717b5369b16aff3573df00bf6
Aziz Kosber <a.kosber@gmail.com>
2018-05-25 20:39:32 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
-def oxford_comma(array)
+require 'pry'
 
+veggies = ["fiddleheads","okra","kohlrabi"]
+def oxford_comma(array)
+  if array.length == 1
+    array[0]
+  elsif array.length == 2
+    array.join(" and ")
+  else
+    array[-1] = "and #{array[-1]}"
+    array.join(", ")
+  end
 end
